f55d385926e3ec30806f47908e05f57e0289e01b
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2021-10-24 20:33:15 -0400

    Initial Commit

---
+++ comp-426-assign-2-programming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  comp-426-assign-2-programming
+//
+//  Created by Serge Dontsa on 2021-10-24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
